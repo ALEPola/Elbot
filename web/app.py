8ce4b623e5e8aa7649972cd0e67ce5f255569110
ALEPola <ale.polanco46@outlook.com>
2025-04-16 15:29:25 -0400

    web update

--- web/app.py
+++ web/app.py
@@ -173,7 +173,8 @@ def webhook_deploy():
 
 # Only for dev
 if __name__=="__main__":
-    app.run(host="0.0.0.0", port=8080, debug=False, use_reloader=False)
+    app.run(host="0.0.0.0", port=5000, debug=True)
+
 
 
 
