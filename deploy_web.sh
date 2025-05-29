fb6f1bc20f2ecc2588f0c0eff15f374c29008705
ALEPola <ale.polanco46@outlook.com>
2025-05-28 20:44:24 -0400

    Add deployment script for web service setup

---
+++ deploy_web.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Ensure the script is run as root
+if [ "$EUID" -ne 0 ]; then
+  echo "Please run as root"
+  exit
+fi
+
+# Activate virtual environment and install Gunicorn
+source /home/pi/ELBOT/venv/bin/activate
+pip install gunicorn
+
+deactivate
+
+# Copy the service file to systemd directory
+cp /home/pi/ELBOT/elbot-web.service /etc/systemd/system/
+
+# Reload systemd and restart the service
+systemctl daemon-reload
+systemctl restart elbot-web.service
+
+# Enable the service to start on boot
+systemctl enable elbot-web.service
+
+# Check the status of the service
+systemctl status elbot-web.service
