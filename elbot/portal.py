4c10f6e04021bf1491b358057d2517277a7dc609
ALEPola <90914390+ALEPola@users.noreply.github.com>
2025-06-11 15:26:15 -0400

    Fix portal import

--- elbot/portal.py
+++ elbot/portal.py
@@ -9,7 +9,14 @@ import threading
 import time
 from pathlib import Path
 
-from flask import Flask, redirect, render_template, request, url_for
+from flask import (
+    Flask,
+    redirect,
+    render_template,
+    request,
+    url_for,
+    render_template_string,
+)
 
 
 ROOT_DIR = Path(__file__).resolve().parents[1]
